2f9077c1da7b385713072fca4567f435b70be41d
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-10-05 16:05:57 -0300

    feat: repository method to find all marks to an specific date

--- src/main/java/com/chronus/app/mark/MarkRepository.java
+++ src/main/java/com/chronus/app/mark/MarkRepository.java
@@ -3,9 +3,20 @@ package com.chronus.app.mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface MarkRepository extends JpaRepository<Mark, Integer> {
-    public List<Mark> getMarkByMarkTime(LocalDateTime dateTime);
+    List<Mark> getMarkByMarkTime(LocalDateTime dateTime);
+
+    List<Mark> findByMarkTimeBetween(LocalDateTime start, LocalDateTime end);
+
+    default List<Mark> getMarkByDate(LocalDateTime dateTime) {
+        var date = dateTime.toLocalDate();
+        var start = LocalDateTime.of(date, LocalTime.of(0, 0, 0));
+        var end = LocalDateTime.of(date, LocalTime.of(23, 59, 59));
+
+        return findByMarkTimeBetween(start, end);
+    };
 }
