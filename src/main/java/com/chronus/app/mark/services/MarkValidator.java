98f58eac4a62b97d5e85aa7231e4f4ea1967d028
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-10-06 21:38:42 -0300

    feat: Validator is verifying valid Marks

--- src/main/java/com/chronus/app/mark/services/MarkValidator.java
+++ src/main/java/com/chronus/app/mark/services/MarkValidator.java
@@ -4,8 +4,10 @@ import com.chronus.app.mark.Mark;
 import com.chronus.app.mark.MarkRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -25,8 +27,11 @@ public class MarkValidator {
     public boolean isValidMarkInterval(Mark mark) {
         List<Mark> dayMarks = repository.getMarkByDate(mark);
 
-        return dayMarks
-                .stream()
-                .anyMatch(e -> e.equals(mark));
+        Mark lastMark = dayMarks.stream()
+                .sorted(Comparator.comparing(Mark::getMarkTime))
+                .toList()
+                .getLast();
+
+        return Duration.between(lastMark.getMarkTime(), mark.getMarkTime()).toHours() < 12;
     }
 }
