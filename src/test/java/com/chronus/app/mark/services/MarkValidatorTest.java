999978cfde1e54205a8dd252c7de58bbf970c92f
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-10-09 22:07:08 -0300

    feat: test to catch

--- src/test/java/com/chronus/app/mark/services/MarkValidatorTest.java
+++ src/test/java/com/chronus/app/mark/services/MarkValidatorTest.java
@@ -111,7 +111,12 @@ public class MarkValidatorTest {
                                 new Mark(user, LocalTime.of(7, 25, 0), date, true, MarkType.ENTRY),
                                 new Mark(user, LocalTime.of(8, 10, 0), date, true, MarkType.EXIT),
                                 new Mark(user, LocalTime.of(12, 0, 0), date, true, MarkType.EXIT)),
-                        new Mark(user, LocalTime.of(8, 45, 0), date, true, MarkType.EXIT), false));
+                        new Mark(user, LocalTime.of(8, 45, 0), date, true, MarkType.EXIT), false),
+                Arguments.of(List.of(
+                                new Mark(user, LocalTime.of(7, 25, 0), date, true, MarkType.ENTRY),
+                                new Mark(user, LocalTime.of(8, 10, 0), date, false, MarkType.EXIT),
+                                new Mark(user, LocalTime.of(12, 0, 0), date, true, MarkType.ENTRY)),
+                        new Mark(user, LocalTime.of(8, 45, 0), date, true, MarkType.EXIT), true));
     }
 
     @ParameterizedTest()
