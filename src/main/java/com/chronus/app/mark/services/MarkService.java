04d95a85d1a6228fd3b1c99153d5364024a0bf2b
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-10-11 00:19:45 -0300

    feat: service message

--- src/main/java/com/chronus/app/mark/services/MarkService.java
+++ src/main/java/com/chronus/app/mark/services/MarkService.java
@@ -6,6 +6,7 @@ import com.chronus.app.utils.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Service
@@ -37,6 +38,10 @@ public class MarkService {
 
         if(!validator.isValidMarkInterval(mark)) {
             return new HttpResponse<Mark>(400, "So much time between Marks!", null);
+        }
+
+        if(validator.isFutureMark(LocalDate.now(), mark)) {
+            return new HttpResponse<Mark>(400, "Cannot insert marks to future dates!", null);
         }
 
         if(!validator.isValidMarkType(mark)) {
