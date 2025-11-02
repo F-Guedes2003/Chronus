16fe3ba35b0ef684759d4e0307469b32008a4019
Aislan <aislan.pepi@outlook.com>
2025-11-02 16:53:27 -0300

    refactor: refatorando codigo frontend

--- frontend/src/MarkForm.tsx
+++ frontend/src/MarkForm.tsx
@@ -2,32 +2,57 @@ import { useState } from 'react'
 import './App.css'
 
 function MarkForm(){
-  const [entry, setEntry] = useState("")
-  const [exit, setExit] = useState("")
+  const [mark, setMark] = useState("")
 
-  function handleChangeEntry(e: any) {
-    setEntry(e.target.value)
+  const [type,setType] = useState("")
+
+  function handleChange(e: any) {
+    setMark(e.target.value)
   }
 
-  function handleChangeExit(e: any){
-    setExit(e.target.value)
+  function handleChangeType(e: any) {
+    setType(e.target.value)
   }
 
   function handleSubmit(e: any) {
     e.preventDefault()
-    console.log(entry,exit)
+    console.log(mark,type)
+    /*fetch('http://localhost:8080/api/v1/marks/mark',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "markTime": "08:00:00",
+        "markDate": "2025-03-03",
+        "type": "ENTRY",
+        "valid": "true",
+        "user": {
+            "id": 28,
+            "name": "Vitor Roque",
+            "email": "vitinbates@sep.com",
+            "password": "password"
+        }
+      })
+      }).then(response => response.text())
+        .then(text => console.log(text))
+        .catch(err => console.log(err.message))*/
   }
 
   return(
     <div className='mark-card'>
       <form onSubmit={handleSubmit}>
         <div className='item-form'>
-          <label>Ponto de Entrada</label>
-          <input type='datetime-local' value={entry} onChange={handleChangeEntry} name='entry-mark' placeholder='Ponto de Entrada'/>
+          <label>Ponto</label>
+          <input type='datetime-local' value={mark} onChange={handleChange} name='mark' placeholder='Ponto de Entrada'/>
         </div>
         <div className='item-form'>
-          <label>Ponto de Saida </label>
-          <input type='datetime-local' value={exit} onChange={handleChangeExit} name='exit-mark' placeholder='Ponto de Saida'/>
+          <label>Tipo do Ponto </label>
+          <select name='mark-type' onSelect={handleChangeType} className='mark-type'>
+            <option value="entry">Entrada</option>
+            <option value="exit">Saida</option>
+          </select>
         </div>
         <button className='bt-submit' type='submit' name='submit-bt'>Enviar Ponto</button>
       </form>
